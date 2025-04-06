d8b0b9a7ebef1299b5d1b352a694423bed9a9c79
vilkova-bite <lalisaviilkova@gmail.com>
2025-04-06 14:57:35 +0500

    поиск и фильтрация по сезону

--- index.js
+++ index.js
@@ -1,30 +1,85 @@
 let SEASON = "зима"
-let winter = document.querySelector ('.winter')
+let winter = document.querySelector ('.winter_btn')
 winter.addEventListener ('click', function (e) {
     e.preventDefault()
     SEASON = 'зима'
-console.log(SEASON)
+    document.querySelectorAll ('.v').forEach(v => {
+
+        v.style.display = 'none'
+    })
+    let winter_list = document.querySelectorAll ('.winter')
+    winter_list.forEach (drink => {
+        drink.style.display = 'block'
+    })
 
     })
-let summer = document.querySelector ('.summer')
+let summer = document.querySelector ('.summer_btn')
 summer.addEventListener ('click', function (e) {
 e.preventDefault()
     SEASON = 'лето'
-console.log(SEASON)
+    document.querySelectorAll ('.v').forEach(v => {
+
+        v.style.display = 'none'
+    })
+    let summer_list = document.querySelectorAll ('.sumer')
+    summer_list.forEach (drink => {
+        drink.style.display = 'block'
+    })
 
     })
-let spring = document.querySelector ('.spring')
+let spring = document.querySelector ('.spring_btn')
 spring.addEventListener ('click', function (e){
 e.preventDefault() 
     SEASON = 'весна'
-console.log(SEASON)
+    document.querySelectorAll ('.v').forEach(v => {
+
+        v.style.display = 'none'
+    })
+    let spring_list = document.querySelectorAll ('.spring')
+    spring_list.forEach (drink => {
+        drink.style.display = 'block'
+    })
+
 
     })
-let autumn = document.querySelector ('.autumn')
+let autumn = document.querySelector ('.autumn_btn')
 autumn.addEventListener ('click', function (e){
 e.preventDefault() 
     SEASON = 'осень'
-console.log(SEASON)
+    document.querySelectorAll ('.v').forEach(v => {
+
+        v.style.display = 'none'
+    })
+    let autumn_list = document.querySelectorAll ('.autumn')
+    autumn_list.forEach (drink => {
+        drink.style.display = 'block'
+    })
 
     })
-console.log(SEASON)
+let drink_names = document.querySelectorAll ('.drink_name')
+
+
+let field_search = document.querySelector (".field-search")
+field_search.addEventListener ('input', function (e) {
+    e.preventDefault()
+    let field_search_value = field_search.value
+    if (field_search_value == ''){
+        drink_names_search = []
+        drink_names.forEach (drink_name => {
+            if (drink_name.includes (field_search_value)) {
+                drink_names_search.push (drink_name)
+            }
+            else{
+                let drink_id = drink_name.getAttribute('id')
+                document.querySelector(`#d${drink_id}`).style.display = 'none'
+                document.querySelector(`#i${drink_id}`).style.display = 'none'
+                
+            }   
+        })
+    }else{
+        document.querySelectorAll ('.v').forEach(v => {
+
+            v.style.display = 'block'
+        })
+    }
+})
